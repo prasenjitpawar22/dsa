c4e7418bbae170ddcb5c6b46fa3f0d7f3d86497e
Prasenjit Pawar <prasenjitpawar22@gmail.com>
2025-06-08 01:20:20 +0530

    Create Undirected-Graph-Cycle-DFS.py

---
+++ Undirected-Graph-Cycle-DFS.py
@@ -0,0 +1,29 @@
+import collections
+''' 
+Undirected graph cycle detection we use - parent - to check if not coming from the save prev vertex.
+'''
+class Solution:
+    def isCycle(self, V, edges):
+        adj = collections.defaultdict(list)
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+
+        visited = set()
+
+        def dfs(v, parent):
+            visited.add(v)
+            for nei in adj[v]:
+                # not visited and dfs check
+                if nei not in visited and dfs(nei, v):
+                        return True
+                # visited and coming from parent
+                elif nei != parent:
+                    return True
+            return False
+
+        for i in range(V):
+            if i not in visited:
+                if dfs(i, -1):
+                    return True
+        return False
